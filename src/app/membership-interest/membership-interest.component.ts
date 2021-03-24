7b7ff9ef5d7b6b4fa7422691b43b8c7d909c6cd1
Drew Yellina <dyellina@iu.edu>
2021-03-24 15:17:08 -0400

    Implementing Form to send email

--- src/app/membership-interest/membership-interest.component.ts
+++ src/app/membership-interest/membership-interest.component.ts
@@ -1,32 +1,39 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Member} from '../data/member';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-
+import {Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 @Component({
   selector: 'app-membership-interest',
   templateUrl: './membership-interest.component.html',
   styleUrls: ['./membership-interest.component.css']
 })
-export class MembershipInterestComponent implements OnInit {
-  @Input() member: Member;
-
-  person = new FormGroup({
-    firstName: new FormControl('', [Validators.required, Validators.pattern(/^[A-Za-z]+$/)]),
-    lastName: new FormControl('', [Validators.required, Validators.pattern(/^[A-Za-z]+$/)]),
-    address: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[A-Za-z0-9])[A-Za-z0-9 _]*$/)]),
-    state: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])[A-Za-z _]*$/)]),
-    city: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])[A-Za-z _]*$/)]),
-    zip: new FormControl('',[Validators.required, Validators.pattern(/^[0-9]{5}/)]),
-    country: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])[A-Za-z _]*$/)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    phoneNumber: new FormControl('', [Validators.required, Validators.pattern(/^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/)]),
-    countryOfResidence: new FormControl('', [Validators.pattern(/^[A-Za-z]+$/)]),
-    referral: new FormControl('', [Validators.pattern(/^[A-Za-z]+$/)])
-  });
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MembershipInterestComponent {
+ public sendEmail(e: Event): void{
+   e.preventDefault();
+   emailjs.sendForm('service_omou9ie', 'template_eriirba', e.target as HTMLFormElement, 'user_ADr0WWYr2ZmiT2o2YDwJW')
+     .then((result: EmailJSResponseStatus) => {
+       console.log(result.text);
+     }, (error) => {
+       console.log(error.text);
+     });
+ }
 }
+
+
+
+
+
+/* FormData: FormGroup;
+ constructor(private builder: FormBuilder , private connectionService: ContactService) {
+   this.FormData = this.builder.group({
+     firstName: ['', Validators.required, Validators.pattern(/^[A-Za-z]+$/)],
+     lastName: ['', Validators.required, Validators.pattern(/^[A-Za-z]+$/)],
+     address: ['', Validators.required, Validators.pattern(/^(?=.*[A-Za-z0-9])[A-Za-z0-9 _]*$/)],
+     state: ['', Validators.required, Validators.pattern(/^(?=.*[A-Za-z])[A-Za-z _]*$/)],
+     city: ['', Validators.required, Validators.pattern(/^(?=.*[A-Za-z])[A-Za-z _]*$/)],
+     zip: ['', Validators.required, Validators.pattern(/^[0-9]{5}/)],
+     country: ['', Validators.required, Validators.pattern(/^(?=.*[A-Za-z])[A-Za-z _]*$/)],
+     email: ['', Validators.required, Validators.email],
+     phoneNumber: ['', Validators.required, Validators.pattern(/^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/)],
+     referral: ['', Validators.pattern(/^[A-Za-z]+$/)]
+   });
+ }
+*/
